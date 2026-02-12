a76e020a50ee6c0cba109d74bbde161dae276a36
vivind08 <vivind875@gmail.com>
2026-02-13 04:10:05 +0530

    Update Script.js

--- Js/Script.js
+++ Js/Script.js
@@ -1,20 +1,27 @@
-function calculateEMI() {
+window.addEventListener("DOMContentLoaded", function () {
 
-    let principal = parseFloat(document.getElementById("loanAmount").value);
-    let annualInterest = parseFloat(document.getElementById("interestRate").value);
-    let tenureYears = parseFloat(document.getElementById("loanTenure").value);
+    const button = document.querySelector(".emi-btn");
 
-    if (isNaN(principal) || isNaN(annualInterest) || isNaN(tenureYears)) {
-        document.getElementById("emiResult").innerHTML = "Please enter valid values";
-        return;
-    }
+    button.addEventListener("click", function () {
 
-    let monthlyInterest = (annualInterest / 12) / 100;
-    let months = tenureYears * 12;
+        let principal = parseFloat(document.getElementById("loanAmount").value);
+        let annualInterest = parseFloat(document.getElementById("interestRate").value);
+        let tenureYears = parseFloat(document.getElementById("loanTenure").value);
 
-    let emi = (principal * monthlyInterest * Math.pow(1 + monthlyInterest, months)) /
-              (Math.pow(1 + monthlyInterest, months) - 1);
+        if (isNaN(principal) || isNaN(annualInterest) || isNaN(tenureYears)) {
+            document.getElementById("emiResult").innerHTML = "Please enter valid values";
+            return;
+        }
 
-    document.getElementById("emiResult").innerHTML =
-        "Your EMI: ₹ " + emi.toLocaleString("en-IN", {maximumFractionDigits: 2});
-}
+        let monthlyInterest = (annualInterest / 12) / 100;
+        let months = tenureYears * 12;
+
+        let emi = (principal * monthlyInterest * Math.pow(1 + monthlyInterest, months)) /
+                  (Math.pow(1 + monthlyInterest, months) - 1);
+
+        document.getElementById("emiResult").innerHTML =
+            "Your EMI: ₹ " + emi.toLocaleString("en-IN", { maximumFractionDigits: 2 });
+
+    });
+
+});
