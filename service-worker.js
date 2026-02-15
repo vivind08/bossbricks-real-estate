225483be99514908509f65afdfa0cb025a7fc584
vivind08 <vivind875@gmail.com>
2026-02-16 03:18:14 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,7 @@
-const CACHE_NAME = "bossbricks-cache-v1";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/login.html",
-  "/builders.html",
-  "/projects.html",
-  "/tax.html",
-  "/style.css"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  console.log("Service Worker Installed");
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-  const urlsToCache = [
-  "/bossbricks-real-estate/index.html",
-  "/bossbricks-real-estate/style.css",
-  "/bossbricks-real-estate/manifest.json",
-  // other assets
-];
-
-      })
-    
-  );
+  event.respondWith(fetch(event.request));
 });
