fa0185808ecfacdbdc54bd39eac6c01e236c7ec8
vivind08 <vivind875@gmail.com>
2026-02-16 03:27:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,10 @@
 self.addEventListener("install", event => {
   console.log("Service Worker Installed");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  console.log("Service Worker Activated");
 });
 
 self.addEventListener("fetch", event => {
